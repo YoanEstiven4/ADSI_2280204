93eda1ba14b3ebc45e836e2cae07c00f82e0a083
Yoan <nospicarnal@gmail.com>
2022-05-09 11:39:27 -0500

    añadiendo carpeta netflix

---
+++ netflix/js/script.js
@@ -0,0 +1,12 @@
+/* function mostrarMensaje(){
+    alert("Ya te viste esta pelicula??");
+} */
+/* function mostrarMensajeCasillas(){
+    alert("Ya te viste la serie..?");
+}
+function intercambiarAurelio(){
+    document.getElementById("imgPrincipal").src = "img/dispa.jpg";
+}*/
+function intercambiarImagen(ruta) {
+    document.getElementById("imgPrincipal").src = ruta;
+}
