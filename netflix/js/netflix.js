09b673f49d0a66b4c5e181c6f33b1c16b91a4d45
Estiven <nospicarnal@gmail.com>
2022-05-14 23:05:34 -0500

    cambio de nombre y carpeta netflix

---
+++ netflix/js/netflix.js
@@ -0,0 +1,15 @@
+/* function mostrarMensaje(){
+    alert("Ya te viste esta pelicula??");
+} */
+/* function mostrarMensajeCasillas(){
+    alert("Ya te viste la serie..?");
+}
+function intercambiarAurelio(){
+    document.getElementById("imgPrincipal").src = "img/dispa.jpg";
+}*/
+function intercambiarImagen(ruta) {
+    document.getElementById("imgPrincipal").src = ruta;
+}
+function mostrarMensajeCasillas(ruta) {
+    document.getElementById("imgPrincipal").src = ruta;
+}
