46eb9ab6d26e8480893ac9b733cf72f6405861e4
Yoan Becerra <alexisbezam@gmail.com>
2022-06-13 11:57:27 -0500

    mi_homecenter en proceso

--- Home_Center/js/home_center.js
+++ Home_Center/js/home_center.js
@@ -7,8 +7,24 @@ let livia = document.getElementById("livia");
 esmeralda.addEventListener("click", function(){
     esmeralda.classList.remove("div-esmeralda");
     esmeralda.classList.add("div-esmeralda-hidden");
+    livia.classList.add("div-livia");
+    livia.classList.remove("div-livia-hidden");
 });
 livia.addEventListener("click", function(){
     livia.classList.remove("div-livia");
     livia.classList.add("div-livia-hidden");
+    esmeralda.classList.add("div-esmeralda");
+    esmeralda.classList.remove("div-esmeralda-hidden");
+});
+maluma.addEventListener("click", function(){
+    maluma.classList.remove("div-maluma");
+    maluma.classList.add("div-maluma-hidden");
+    jose.classList.add("div-jose");
+    jose.classList.remove("div-jose-hidden");
+});
+jose.addEventListener("click", function(){
+    jose.classList.remove("div-jose");
+    jose.classList.add("div-jose-hidden");
+    maluma.classList.add("div-maluma");
+    maluma.classList.remove("div-maluma-hidden");
 });
